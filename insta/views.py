302eb40dec9c4bf150073ea77cc5874902a35821
ABBY <abbyW@moringa.com>
2022-06-05 17:15:02 +0300

    Added logout functionality.

--- insta/views.py
+++ insta/views.py
@@ -6,32 +6,42 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def register(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method =='POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request, "Account for" + user + "Created Successfully!")
 
-    if request.method =='POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user=form.cleaned_data.get('username')
-            messages.success(request, "Account for" + user + "Created Successfully!")
-
-            return redirect('login')
+                return redirect('login')
 
 
-    return render(request, 'authenticate/register.html',{'form':form} )
+        return render(request, 'authenticate/register.html',{'form':form} )
       
-def loginPage(request):
-    if request.method =='POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method =='POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
 
-        else:
-            messages.info(request, 'Check username or password and try again')
+            else:
+                messages.info(request, 'Check username or password and try again')
 
-    return render(request, 'authenticate/login.html' )
+        return render(request, 'authenticate/login.html' )
+
+def logoutUser(request):
+    logout(request)
+
+    return redirect('login')
