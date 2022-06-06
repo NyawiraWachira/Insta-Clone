5250960a214c38b41225906736ea4461ca31d1c5
ABBY <abbyW@moringa.com>
2022-06-06 15:28:15 +0300

    Styled the template for updating user details.

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta.apps.InstaConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- insta/models.py
+++ insta/models.py
@@ -7,7 +7,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField(default='default.png', upload_to='profile_pics')
-    bio = models.TextField()
+    bio = models.TextField(max_length =300)
 
         
     def __str__(self):
@@ -16,34 +16,3 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
     
-        # img = Image.open(self.image.path)
-
-        # if img.height > 300 or img.width > 300:
-        #     output_size = (300, 300)
-        #     img.thumbnail(output_size)
-        #     img.save(self.image.path)   
-
-
-# class Image(models.Model):
-#     image = models.ImageField(null=False, blank=False)
-#     image_name = models.CharField(max_length =30)
-#     image_caption = models.TextField()
-#     profile = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
-#     post_date = models.DateTimeField(auto_now_add=True)
-    # likes=
-    # comments=
-
-    
-
-    # def __str__(self):
-    #     return self.image_name
-
-# def create_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         Profile.objects.create(user=instance)
-
-    # def save_profile(sender, instance, **kwargs):
-    #     instance.profile.save()
-
-    # def save(self):
-    #     super().save()
