bcc733fcc004debdc92b72f954dd0205598cf1fd
ABBY <abbyW@moringa.com>
2022-06-06 12:38:52 +0300

    Made few changes onprofile model and made migrations.

--- insta/forms.py
+++ insta/forms.py
@@ -9,3 +9,14 @@ class CreateUserForm(UserCreationForm):
         model=User
         fields=['email','username','password1', 'password2']
 
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_photo','bio']
