f5fc2c41748eced40656c1ed3cd0e5465c4d6fa7
ABBY <abbyW@moringa.com>
2022-06-07 10:42:52 +0300

    Created form for users to create new posts.

--- insta/forms.py
+++ insta/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile,Post
 
 
 class CreateUserForm(UserCreationForm):
@@ -20,3 +20,12 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_photo','bio']
+
+class NewPostForm(forms.ModelForm):
+	picture = forms.ImageField(required=True)
+	caption = forms.CharField(widget=forms.Textarea(attrs={'class': 'input is-medium'}), required=True)
+	tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input is-medium'}), required=True)
+
+	class Meta:
+		model = Post
+		fields = ('picture', 'caption', 'tags')
